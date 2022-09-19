f315c8bae49891e9a779aee3c4f4336c5b1eb7d8
Okereke Chiamaka Jennifer <110993288+jennysaggesse@users.noreply.github.com>
2022-09-19 17:49:14 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- * Description - prints "\"Programming is like a multilingual puzzle"
- * Return: success (0)
+ * Description: main - Print out a string to stdout.
+ * Return: 0 if successfull.
  */
-
 int main(void)
 {
-	puts("\" Programming is like building multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
